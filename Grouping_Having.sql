c50e40aa47d50906b29dd0c0a962cefa728187c0
Bamidele Adeola <ajamubamidele@gmail.com>
2022-10-26 22:02:48 +0100

    update on aggregate, groupby and  having

--- Grouping_Having.sql
+++ Grouping_Having.sql
@@ -48,3 +48,72 @@ HAVING
   SUM(demand_loss_mw) > 10000 
 ORDER BY 
   demand_loss DESC;
+
+
+ ------------------------------------------------------------------------
+/* Q3 Use MIN and MAX to retrieve the minimum and maximum values for the place and points columns respectively.*/
+-------------------------------------------------------------------------
+
+  -- Retrieve the minimum and maximum place, and point values
+SELECT 
+  -- the lower the value the higher the place, so MIN = the highest placing
+  MIN(place) AS hi_place, 
+  MAX(place) AS lo_place, 
+  -- Retrieve the minimum and maximum points values. This time MIN = the lowest points value
+  MIN(points) AS min_points, 
+  MAX(points) AS max_points 
+FROM 
+  eurovision;
+
+
+  -- Q3B Let's obtain more insight from our results by adding a GROUP BY clause. Group the results by country.
+  -- Retrieve the minimum and maximum place, and point values
+SELECT 
+  -- the lower the value the higher the place, so MIN = the highest placing
+  MIN(place) AS hi_place, 
+  MAX(place) AS lo_place,  
+  -- Retrieve the minimum and maximum points values. This time MIN = the lowest points value
+  MIN(points) AS min_points, 
+  MAX(points) AS max_points 
+FROM 
+  eurovision
+  -- Group by country
+GROUP BY
+  country;
+
+  -- Q3C Add COUNT OF country, country column to the GROUP BY clause of the previous query
+
+  -- Obtain a count for each country
+SELECT 
+  COUNT (country) AS country_count, 
+  -- Retrieve the country column
+  country, 
+  -- Return the average of the Place column 
+  AVG(place) AS average_place, 
+  AVG(points) AS avg_points, 
+  MIN(points) AS min_points, 
+  MAX(points) AS max_points 
+FROM 
+  eurovision 
+GROUP BY 
+  country;
+
+  -- 3D Apply a filter so we only return rows where the country_count is greater than 5. Then arrange by avg_place in ascending order, and avg_points in descending order.
+  SELECT 
+  country, 
+  COUNT (country) AS country_count, 
+  AVG (place) AS avg_place, 
+  AVG (points) AS avg_points, 
+  MIN (points) AS min_points, 
+  MAX (points) AS max_points 
+FROM 
+  eurovision 
+GROUP BY 
+  country 
+  -- The country column should only contain those with a count greater than 5
+HAVING 
+  COUNT(country) > 5 
+  -- Arrange columns in the correct order
+ORDER BY 
+  avg_place, 
+  avg_points DESC;
