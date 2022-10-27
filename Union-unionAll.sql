e1dee293b3a1e583dd7156c7c2a294a39e60f6cd
Bamidele Adeola <ajamubamidele@gmail.com>
2022-10-27 07:23:07 +0100

    modified union and union all

--- Union-unionAll.sql
+++ Union-unionAll.sql
@@ -21,3 +21,22 @@ SELECT
   'Artist'  AS Source
   -- Complete the FROM statement
 FROM Artist;
+
+/*-- Q2
+Make the first selection from the album table. 
+Then join the results by providing the relevant keyword and selecting from the artist table keeping duplicates*/
+
+SELECT 
+  album_id AS ID,
+  title AS description,
+  'Album' AS Source
+  -- Complete the FROM statement
+FROM Album
+ -- Combine the result set using the relevant keyword
+UNION ALL  --union all helps to keep all duplicates
+SELECT 
+  artist_id AS ID,
+  name AS description,
+  'Artist'  AS Source
+  -- Complete the FROM statement
+FROM Artist;
