c5061820c89cfecd51bc8fe18abfd6e0d2fa4ef7
Bamidele Adeola <ajamubamidele@gmail.com>
2022-10-27 07:20:31 +0100

    union and union all query

---
+++ Union-unionAll.sql
@@ -0,0 +1,23 @@
+-- 7 -- 
+------------------------------
+-- UNION AND UNION ALL
+------------------------------
+
+/*-- Q1
+Make the first selection from the album table. 
+Then join the results by providing the relevant keyword and selecting from the artist table*/
+
+SELECT 
+  album_id AS ID,
+  title AS description,
+  'Album' AS Source
+  -- Complete the FROM statement
+FROM Album
+ -- Combine the result set using the relevant keyword
+UNION
+SELECT 
+  artist_id AS ID,
+  name AS description,
+  'Artist'  AS Source
+  -- Complete the FROM statement
+FROM Artist;
