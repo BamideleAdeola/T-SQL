1f1844804133694d9b040b96b38b7a6ede61abee
Bamidele Adeola <ajamubamidele@gmail.com>
2023-02-04 15:33:56 +0100

    User defined function

---
+++ Create_user_defined_function.sql
@@ -0,0 +1,15 @@
+
+/*Create a scalar function that adds 1000 to a given input in SQL server*/
+
+CREATE FUNCTION dbo.Add1000 (@var INT)
+RETURNS INT AS
+BEGIN
+	RETURN @var + 1000
+END
+GO
+
+
+-- Select the function with 200 as parameter, this should return 1200
+
+SELECT dbo.Add1000(200)
+GO
